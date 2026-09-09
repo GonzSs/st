90a5ad2151735c1448d8a9704542891481651fb1
Gonzalo <gonzalo.sosa@proton.me>
2026-09-08 22:06:29 -0300

    fix(netbook): restore static colorname array and cursor colors

--- config.h
+++ config.h
@@ -109,6 +109,45 @@ static unsigned int cursorshape = 2;
 static unsigned int cols = 80;
 static unsigned int rows = 24;
 
+/* Terminal colors (16 first used in escape sequence) */
+static const char *colorname[] = {
+    /* 8 normal colors */
+    "black",
+    "red3",
+    "green3",
+    "yellow3",
+    "blue2",
+    "magenta3",
+    "cyan3",
+    "gray90",
+
+    /* 8 bright colors */
+    "gray50",
+    "red",
+    "green",
+    "yellow",
+    "#5c5cff",
+    "magenta",
+    "cyan",
+    "white",
+
+    [255] = 0,
+
+    /* more colors can be added after 255. */
+    "gray90", /* default foreground */
+    "black",  /* default background */
+    "#555555",
+};
+
+/*
+ * Default colors (colorname index)
+ * foreground, background, cursor, reverse cursor
+ */
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 256;
+static unsigned int defaultrcs = 257;
+
 /*
  * Default colour and shape of the mouse cursor
  */
